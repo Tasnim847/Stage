43cfc9e4f003e78a6d9ec82b6cce5934ffeaf734
Tasnim847 <tasnim.ghodbane@esprit.tn>
2025-09-03 12:10:32 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,5 +7,17 @@ export default defineConfig({
   },
   css: {
     postcss: './postcss.config.cjs'
+  },
+  build: {
+    chunkSizeWarningLimit: 1000, // Augmente la limite d'avertissement
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts'],
+          ui: ['@mui/material', '@mui/icons-material', 'antd']
+        }
+      }
+    }
   }
 });
